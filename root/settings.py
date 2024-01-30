ea53a62e2315a8893c62977deddfdc777e75de9d
adrianocastanheira <adriano.castanheira@gmail.com>
2024-01-30 17:01:27 -0300

    corrige url e root do settings

--- root/settings.py
+++ root/settings.py
@@ -144,11 +144,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
