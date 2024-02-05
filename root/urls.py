1b55ca958fa61616416d10a94d644fe99581ce06
tiberio <tiberio@gmail.com>
2024-02-05 12:14:22 -0300

    Atualiza urls

--- root/urls.py
+++ root/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
     path('cards/', include('cards.urls')),
     path('compras/', include('compras.urls')),
-    path('admin/', admin.site.urls),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
